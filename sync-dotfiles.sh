3f70f88ea75c87adb82214d6682d75c274d4d359
Sano <22659443+ordy@users.noreply.github.com>
2021-07-13 15:25:43 +0200

    2021-07-13 Update

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -3,16 +3,16 @@
 DIR=$(dirname $(readlink -f $0))
 ISODATE=$(date --iso)
 
-rsync -P $HOME/{.zshrc,.p10k.zsh,.bash_profile,.bashrc,.dir_colors,.vimrc,.xbindkeysrc,.Xresources,.imwheelrc,.alacritty.yml} $DIR
-rsync -P $HOME/.ncmpcpp/config $DIR/.ncmpcpp/
-rsync -P $HOME/.config/picom.conf $DIR/config/
-rsync -P $HOME/.config/mpd/mpd.conf $DIR/.config/mpd/
-rsync -aP $HOME/.bin $DIR
-rsync -aP $HOME/.config/fontconfig $DIR/.config/
-rsync -aP $HOME/.config/sway $DIR/.config/
-rsync -aP $HOME/.config/waybar $DIR/.config/
-rsync -aP $HOME/.config/wlogout $DIR/.config/
-rsync -aP $HOME/.config/kanshi $DIR/.config/
+rsync -P --delete-after $HOME/{.zshrc,.p10k.zsh,.bash_profile,.bashrc,.dir_colors,.vimrc,.xbindkeysrc,.Xresources,.imwheelrc,.alacritty.yml} $DIR
+rsync -P --delete-after $HOME/.ncmpcpp/config $DIR/.ncmpcpp/
+rsync -P --delete-after $HOME/.config/picom.conf $DIR/config/
+rsync -P --delete-after $HOME/.config/mpd/mpd.conf $DIR/.config/mpd/
+rsync -aP --delete-after $HOME/.bin $DIR
+rsync -aP --delete-after $HOME/.config/fontconfig $DIR/.config/
+rsync -aP --delete-after $HOME/.config/sway $DIR/.config/
+rsync -aP --delete-after $HOME/.config/waybar $DIR/.config/
+rsync -aP --delete-after $HOME/.config/wlogout $DIR/.config/
+rsync -aP --delete-after $HOME/.config/kanshi $DIR/.config/
 
 git -C $DIR add .
 git -C $DIR commit -m "$ISODATE Update"
