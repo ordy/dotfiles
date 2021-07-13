1526d94babbd8b1f09e0d63b0fe75c8a20d5efab
Sandro Tabak <ordysan@gmail.com>
2021-07-13 15:03:52 +0200

    2021-07-13 Update

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-rsync -v $HOME/{.zshrc,.p10k.zsh,.bash_profile,.bashrc,.dir_colors,.vimrc,.xbindkeysrc,.Xresources,.imwheelrc,.alacritty.yml} .
-rsync -v $HOME/.ncmpcpp/config ./.ncmpcpp/
-rsync -v $HOME/.config/picom.conf ./.config/
-rsync -v $HOME/.config/mpd/mpd.conf ./.config/mpd/
-rsync -av $HOME/.bin .
-rsync -av $HOME/.config/fontconfig ./.config/
-rsync -av $HOME/.config/sway ./.config/
-rsync -av $HOME/.config/waybar ./.config/
-rsync -av $HOME/.config/wlogout ./.config/
-rsync -av $HOME/.config/kanshi ./.config/
+DIR=$(dirname $(readlink -f $0))
+ISODATE=$(date --iso)
+
+rsync -P $HOME/{.zshrc,.p10k.zsh,.bash_profile,.bashrc,.dir_colors,.vimrc,.xbindkeysrc,.Xresources,.imwheelrc,.alacritty.yml} $DIR
+rsync -P $HOME/.ncmpcpp/config $DIR/.ncmpcpp/
+rsync -P $HOME/.config/picom.conf $DIR/config/
+rsync -P $HOME/.config/mpd/mpd.conf $DIR/.config/mpd/
+rsync -aP $HOME/.bin $DIR
+rsync -aP $HOME/.config/fontconfig $DIR/.config/
+rsync -aP $HOME/.config/sway $DIR/.config/
+rsync -aP $HOME/.config/waybar $DIR/.config/
+rsync -aP $HOME/.config/wlogout $DIR/.config/
+rsync -aP $HOME/.config/kanshi $DIR/.config/
+
+git -C $DIR add .
+git -C $DIR commit -m "$ISODATE Update" 
