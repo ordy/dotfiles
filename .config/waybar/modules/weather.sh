28e84837a4a22676d381da4ccbe70d1b2aa262b5
Sano <22659443+ordy@users.noreply.github.com>
2022-04-26 03:22:17 +0200

    2022-04-26 Update

--- .config/waybar/modules/weather.sh
+++ .config/waybar/modules/weather.sh
@@ -1,38 +1,31 @@
 #!/bin/bash
 
-location='Brussels,BE'
-
 # Set up caching to avoid tons of reqs to wttr
 cachedir=~/.cache/rbn
-cachefile=${0##*/}-$location
+cachefile=${0##*/}
 
 if [ ! -d $cachedir ]; then
-    mkdir -p $cachedir
+  mkdir -p $cachedir
 fi
 
 if [ ! -f $cachedir/$cachefile ]; then
-    touch $cachedir/$cachefile
+  touch $cachedir/$cachefile
 fi
 
 # Save current IFS
 SAVEIFS=$IFS
 # Change IFS to new line.
-IFS=$'\n'
+IFS=$'\n' 
 
 cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
 if [ $cacheage -gt 1740 ] || [ ! -s $cachedir/$cachefile ]; then
-    data=($(curl -s https://en.wttr.in/$location\?0qnT 2>&1))
-    echo ${data[0]} | cut -f1 -d, > $cachedir/$cachefile
-    echo ${data[1]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
-    echo ${data[2]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
+  data=($(curl -s https://en.wttr.in\?format\="%t;+%f;+%w;+%C;+%h;+%m;+%p;+%l" 2>&1))
+  echo $data > $cachedir/$cachefile
 fi
 
-weather=($(cat $cachedir/$cachefile))
+IFS=';' read -r -a wdata <<< $(cat $cachedir/$cachefile)
 
 # Restore IFSClear
 IFS=$SAVEIFS
 
-temperature=$(echo ${weather[2]} | sed -E 's/([[:digit:]])+\.\./\1 to /g')
-#temperature=$(echo ${weather[2]} | sed 's/[^0-9]//g')
-
-echo -e "{\"text\":\"$temperature\", \"class\": \"weather\", \"alt\":\""${weather[0]} ${weather[1]##*,} "\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
+echo -e "{\"text\":\"${wdata[0]}\",\"class\":\"weather\",\"alt\":\"${wdata[7]}\",\"tooltip\":\"${wdata[3]}${wdata[0]}\\\n Feels like: ${wdata[1]}\\\n Wind: ${wdata[2]}\\\n Humidity: ${wdata[4]}\\\n Precipitation: ${wdata[6]}\"}"
