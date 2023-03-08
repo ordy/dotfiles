af3cb362f4c3e03168df3cad451e61c09954abb4
Sano <22659443+ordy@users.noreply.github.com>
2023-03-08 13:41:10 +0100

    2023-03-08 Update

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      file_ignore_patterns = { "Music", "Videos", "Pictures", "Documents", "pso2", "node_modules" },
+    },
+  },
+}
