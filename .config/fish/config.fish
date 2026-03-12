d9b65586871deb222d390943f22bcaaf1d2caff2
Sano <22659443+ordy@users.noreply.github.com>
2026-03-12 13:37:39 +0100

    2026-03-12 Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,10 @@
 source $__fish_config_dir/environment.fish
 source $__fish_config_dir/alias.fish
-
 set -U fish_key_bindings fish_default_key_bindings
 
-set --global tide_pwd_color_dirs 8FAEC2
-set --global tide_pwd_color_anchors A7C4D6
-set --global tide_left_prompt_prefix ''
-set --global tide_character_icon ''
-set --global tide_os_icon ''
-set --global tide_character_color FFFFFF
-set --global tide_git_color_branch 76946A
-set --global tide_git_color_dirty DCA561
-set --global tide_git_color_untracked 7E9CD8
+set -g tide_left_prompt_prefix ''
+set -g tide_character_icon ''
+set -g tide_os_icon ''
 
 fish_add_path /home/sano/.spicetify
+fish_config theme choose "Rosé Pine"
