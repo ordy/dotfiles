1858024e209fc44b74870eed8bbd7a3b3543f890
Sano <22659443+ordy@users.noreply.github.com>
2026-03-19 10:29:40 +0100

    2026-03-19 Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,9 @@ source $__fish_config_dir/environment.fish
 source $__fish_config_dir/alias.fish
 set -U fish_key_bindings fish_default_key_bindings
 
-set -g tide_left_prompt_prefix ' '
-set -g tide_character_icon ' '
-set -g tide_os_icon ''
+set -g tide_left_prompt_prefix ' '
+set -g tide_character_icon ''
+set -g tide_left_prompt_items pwd git newline character
 
 fish_add_path /home/sano/.spicetify
 fish_config theme choose "Rosé Pine"
