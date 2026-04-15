25daba5f1345420a1cc1e5e2660db6df615230de
Sano <22659443+ordy@users.noreply.github.com>
2026-04-15 21:15:14 +0200

    2026-04-15 Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,5 @@ set -g tide_left_prompt_prefix '󰧱 '
 set -g tide_character_icon '󰧱'
 set -g tide_left_prompt_items pwd git newline character
 
-fish_add_path /home/sano/.spicetify
+fish_add_path ~/.spicetify
 fish_config theme choose "Rosé Pine"
